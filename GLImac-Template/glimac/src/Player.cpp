ee166205f5fc0497ea83f6473bc18785a7e7fdce
COMBE AUDREY <acombe@localhost.localdomain>
2018-12-17 12:20:34 +0100

    Player.cpp

--- GLImac-Template/glimac/src/Player.cpp
+++ GLImac-Template/glimac/src/Player.cpp
@@ -6,3 +6,12 @@
 
 
 #include "Player.hpp"
+
+
+Player::Player()
+ :m_pseudo(""), m_score(0), m_money(0), m_model(""){}
+
+Player::Player(std::string &pseudo, Model &model)
+ :m_pseudo(pseudo), m_score(0), m_money(0), m_model(model){}
+
+Player::~Player(){}
