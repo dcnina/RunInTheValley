d9adce832425823c42d56312849200162698d039
audreycombe <acombe@etud.u-pem.fr>
2018-12-17 22:18:44 +0100

    debut map

--- GLImac-Template/glimac/include/glimac/Map.hpp
+++ GLImac-Template/glimac/include/glimac/Map.hpp
@@ -34,17 +34,53 @@ class Map{
 		///CONSTRUCTOR
 		/**
 		*\brief Default constructor   
-		*\details Constructor by default of Bloc
+		*\details Constructor by default of Map
 		*/
 		Map();
 
+		/**
+		*\brief Map Constructor
+		*\details Constructor of Map
+		*/
+		Map(const std::vector<Bloc> &listBlocs, const std::vector<Enemy> &listEnemies);
 
 		/// GETTERS
 		/**
-		*\brief Getter World of the Game   
-		*\return current World 
+		*\brief Get list of blocks of the map   
+		*\return current list of blocks
 		*/
+		inline std::vector<Bloc> getListBlocs() const{ return m_listBlocs; }
 
+		/**
+		*\brief Get list of enemies of the map   
+		*\return current list of enemies
+		*/
+		inline std::vector<Enemy> getListEnemies() const{ return m_listEnemies; }
+
+		///SETTERS
+		/**
+		*\brief Set listBlocs of the map
+		*/
+		inline void setListBlocs(const std::vector<Bloc> &listBlocs) { 
+			for(unsigned int i=0; i<listBlocs.size(); i++)
+				m_listBlocs[i] = listBlocs[i];
+		}
+
+		/**
+		*\brief Set listEnemies of the map
+		*/
+		inline void setListEnemies(const std::vector<Enemy> &listEnemies) { 
+			for(unsigned int i=0; i<listEnemies.size(); i++)
+				m_listEnemies[i] = listEnemies[i];
+		}
+
+		///METHODS
+		/**
+		*\brief initialise list of Blocks   
+		*\details initialise list of blocks from file
+		*\param file
+		*/
+		void initialiseListBlocFromFile(const char* filename);
 		
 		//DESTRUCTOR
 		/**
--- GLImac-Template/glimac/src/Map.cpp
+++ GLImac-Template/glimac/src/Map.cpp
@@ -6,3 +6,45 @@
 
 
 #include <glimac/Map.hpp>
+#include <iostream>
+#include <fstream>
+
+Map::Map(){
+	for(unsigned int i = 0; i < m_listBlocs.size(); i++)
+		m_listBlocs[i] = Bloc();
+
+	for(unsigned int i = 0; i < m_listEnemies.size(); i++)
+		m_listEnemies[i] = Enemy();
+}
+
+Map::Map(const std::vector<Bloc> &listBlocs, const std::vector<Enemy> &listEnemies){
+	for(unsigned int i = 0; i < listBlocs.size(); i++)
+		m_listBlocs[i] = listBlocs[i];
+
+	for(unsigned int i = 0; i < listEnemies.size(); i++)
+		m_listEnemies] = listEnemies[i];
+}
+
+void Map::initialiseListBlocFromFile(const char* filename){
+	/*fstream file;
+
+	file.open(filename, ios::in);
+
+	if(!file){
+		////////////exception
+		std::cout << "error in opening file " << std::endl;
+		return 0;
+	}
+
+	char ch;
+	int nbLines = 0;
+
+	nbLines = fgetc(file);
+
+	for(int i = 0; i<nbLines; i++){
+		
+	}*/
+}
+
+
+Map::~Map(){}
