d8d3ff03b6450f471af0471553907ecf3269d78f
COMBE AUDREY <acombe@localhost.localdomain>
2018-12-17 13:20:48 +0100

    player.cpp

---
+++ GLImac-Template/glimac/src/Player.cpp
@@ -0,0 +1,17 @@
+/*
+ * COMBE Audrey, DE CASTRO Nina, LAVALLE Lucas
+ * IMAC2 - TD2 
+ * RunInTheValley - Player.cpp 
+ */
+
+
+#include <glimac/Player.hpp>
+
+
+Player::Player()
+ :m_pseudo(""), m_score(0), m_money(0), m_model(""){}
+
+Player::Player(std::string &pseudo, Model &model)
+ :m_pseudo(pseudo), m_score(0), m_money(0), m_model(model){}
+
+Player::~Player(){}
