4a08a25ef0105b40d7bb8a1ebf4ed6213d82d757
LucasLavallee <lucas.lavallee@outlook.com>
2018-12-19 12:23:23 +0100

    Bug correct Object.cpp

--- GLImac-Template/glimac/src/Object.cpp
+++ GLImac-Template/glimac/src/Object.cpp
@@ -7,10 +7,10 @@
 
 #include <glimac/Object.hpp>
 
-Object::Object(){
+/*Object::Object(){
 	for(int i=0; i<3; i++)
 		m_position[i] = 0;
-}
+}*/
 
 Object::Object(const std::vector<double> &position){
 	for(unsigned int i=0; i<position.size(); i++)
